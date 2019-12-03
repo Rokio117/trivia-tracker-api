27f1a9a19d7e29aa7e434a38926e9e044fc8093a
Nick Hillary <hillan02@gmail.com>
2019-12-02 21:42:51 -0600

    finished /user routes

--- src/users/users-service.js
+++ src/users/users-service.js
@@ -9,6 +9,46 @@ const usersService = {
   },
   getUser(userName) {
     return store.users.find(user => user.userName === userName);
+  },
+  getNameFromUserName(userName) {
+    return store.users.find(user => user.userName === userName).name;
+  },
+  getUserTeams(userName) {
+    return store.teams.filter(team =>
+      team.members.map(member => member.userName).includes(userName)
+    );
+  },
+  getUserProfile(userName) {
+    return store.users.find(user => user.userName === userName);
+  },
+  postNewUserNoTeam(profile) {
+    return store.users.push(profile);
+  },
+  changeUserName(newUserName, userName) {
+    store.users.find(user => user.userName === userName).userName = newUserName;
+
+    store.teams
+      .filter(team => team.members.find(member => member.userName === userName))
+      .forEach(
+        team =>
+          (team.members.find(
+            member => member.userName === userName
+          ).userName = newUserName)
+      );
+
+    store.teams
+      .filter(team =>
+        team.members.find(member => member.userName === newUserName)
+      )
+      .forEach(team =>
+        team.history.forEach(event => {
+          const index = event.roster.indexOf(userName);
+          event.roster.splice(index, 1, newUserName);
+        })
+      );
+  },
+  changePlayerName(newName, userName) {
+    store.users.find(user => user.userName === userName).name = newName;
   }
 };
 
--- src/users/users.js
+++ src/users/users.js
@@ -1,16 +1,95 @@
 const express = require("express");
 const userService = require("./users-service");
 const usersRouter = express.Router();
+const jsonBodyParser = express.json();
 
-usersRouter.route("/").get((req, res, next) => {
-  res.json(userService.getAllusers());
-});
+usersRouter
+  .route("/")
+  .get((req, res, next) => {
+    res.json(userService.getAllusers());
+  })
+  .post(jsonBodyParser, (req, res, next) => {
+    console.log(req.body);
+    const { userName, name, password } = req.body;
+    const newUser = {
+      userName: userName,
+      name: name,
+      password: password
+    };
+    for (const [key, value] of Object.entries(newUser))
+      if (value == (null || ""))
+        return res.status(400).json({
+          error: `Missing '${key}' in request body`
+        });
+    if (userService.userExists(userName)) {
+      return res.status(400).json({ error: "User already exists" });
+    }
 
-usersRouter.route("/:user_name").get((req, res, next) => {
+    userService.postNewUserNoTeam(newUser);
+    return res.json(userService.getAllusers());
+  });
+
+usersRouter
+  .route("/:user_name")
+  .get((req, res, next) => {
+    if (!userService.userExists(req.params.user_name)) {
+      res.status(404).json({ error: "User does not exist" });
+    } else {
+      res.json(userService.getUser(req.params.user_name));
+    }
+  })
+  .patch(jsonBodyParser, (req, res, next) => {
+    const { newUserName } = req.body;
+    const newUser = {
+      userName: newUserName
+    };
+    if (!userService.userExists(req.params.user_name)) {
+      return res.status(404).json({ error: "User does not exist" });
+    }
+    for (const [key, value] of Object.entries(newUser))
+      if (value == (null || ""))
+        return res.status(400).json({
+          error: `Missing '${key}' in request body`
+        });
+    userService.changeUserName(newUser.userName, req.params.user_name);
+    return res.json(userService.getAllusers());
+  });
+
+usersRouter
+  .route("/:user_name/name")
+  .get((req, res, next) => {
+    if (!userService.userExists(req.params.user_name)) {
+      res.status(404).json({ error: "User does not exist" });
+    } else {
+      res.json(userService.getNameFromUserName(req.params.user_name));
+    }
+  })
+  .patch(jsonBodyParser, (req, res, next) => {
+    if (!userService.userExists(req.params.user_name)) {
+      return res.status(404).json({
+        error: "User does not exist"
+      });
+    }
+    if (!req.body.name || req.body.name === "") {
+      res.status(404).json({ error: "Missing name in request body" });
+    }
+    userService.changePlayerName(req.body.name, req.params.user_name);
+    res.json(userService.getUser(req.params.user_name));
+  });
+
+usersRouter.route("/:user_name/teams").get((req, res, next) => {
   if (!userService.userExists(req.params.user_name)) {
     res.status(404).json({ error: "User does not exist" });
   } else {
-    res.json(userService.getUser(req.params.user_name));
+    res.json(userService.getUserTeams(req.params.user_name));
+  }
+});
+
+usersRouter.route("/:user_name/profile").get((req, res, next) => {
+  if (!userService.userExists(req.params.user_name)) {
+    res.status(404).json({ error: "User does not exist" });
+  } else {
+    res.json(userService.getUserProfile(req.params.user_name));
   }
 });
 
