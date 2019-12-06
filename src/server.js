7aa7a6ea6b35965ab9c981552c103142012eb9f6
Nick Hillary <hillan02@gmail.com>
2019-12-06 11:04:20 -0600

    added migrations and first table migrate

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = require("./app");
+const knex = require("knex");
+const { PORT, DB_URL } = require("./config");
 
-const PORT = process.env.PORT || 3000;
+const db = knex({
+  client: "pg",
+  connection: DB_URL
+});
 
-// app.get("/api/*", (req, res) => {
-//   res.json({ ok: true });
-// });
+app.set("db", db);
 
-// app.get("/api/", (req, res) => {
-//   res.send("Hello, world!");
-// });
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
