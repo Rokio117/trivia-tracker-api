5be8eb41e00f6ab5c53a4f18f475567fa0c99c75
Nick Hillary <hillan02@gmail.com>
2019-12-09 17:22:16 -0600

    added drop table script

--- migrations/001.do.create_trivia_players.sql
+++ migrations/001.do.create_trivia_players.sql
@@ -1,3 +1,13 @@
+DROP TABLE IF EXISTS trivia_players CASCADE;
+DROP TABLE IF EXISTS trivia_teams CASCADE;
+DROP TABLE IF EXISTS members;
+DROP TABLE IF EXISTS trivia_locations;
+DROP TABLE IF EXISTS trivia_events;
+DROP TABLE IF EXISTS trivia_results;
+DROP TYPE IF EXISTS options;
+DROP TABLE IF EXISTS trivia_attendees;
+
+
 
 
 CREATE TABLE IF NOT EXISTS trivia_players (
--- seeds/drop.tables.sql
+++ seeds/drop.tables.sql
@@ -1,8 +1,9 @@
 DROP TABLE IF EXISTS trivia_players CASCADE;
-DROP TABLE IF EXISTS trivia_teams;
+DROP TABLE IF EXISTS trivia_teams CASCADE;
 DROP TABLE IF EXISTS members;
 DROP TABLE IF EXISTS trivia_locations;
 DROP TABLE IF EXISTS trivia_events;
 DROP TABLE IF EXISTS trivia_results;
 DROP TYPE IF EXISTS options;
 DROP TABLE IF EXISTS trivia_attendees;
+DROP TABLE IF EXISTS schemaversion;
