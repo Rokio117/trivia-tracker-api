02090ee69a228920afbce1cda1b556697f881713
Nick Hillary <hillan02@gmail.com>
2019-12-10 16:29:31 -0600

    first test set up

--- test/users-endpoints.spec.js
+++ test/users-endpoints.spec.js
@@ -1,10 +1,17 @@
 const knex = require("knex");
 const app = require("../src/app");
 const { cleanTables } = require("./helpers");
+const { seedData } = require("./seedData");
 
-describe("Users Endpoints", function() {
+describe.only("Users Endpoints", function() {
   let db;
-
+  const users = seedData.users();
+  const teams = seedData.teams();
+  const members = seedData.members();
+  const locations = seedData.locations();
+  const events = seedData.events();
+  const results = seedData.results();
+  const attendees = seedData.attendees();
   before("make knex instance", () => {
     db = knex({
       client: "pg",
@@ -16,4 +23,14 @@ describe("Users Endpoints", function() {
   after("disconnect from db", () => db.destroy());
   before("clean the tables", () => cleanTables(db));
   afterEach("clean up", () => cleanTables(db));
+
+  descrite(`Get /api/teams/`, () => {
+    context(`Given no articles`, () => {
+      it(`responds with an empty list`, () => {
+        return supertest(app)
+          .get(`/api/teams/`)
+          .expect([]);
+      });
+    });
+  });
 });
