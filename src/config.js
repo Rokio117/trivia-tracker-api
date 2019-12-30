2f1ea809f5378c6e325443cadc48c730bc7629e0
Nick Hillary <hillan02@gmail.com>
2019-12-30 16:59:55 -0600

    changed DB_URL to DATASE_URL

--- src/config.js
+++ src/config.js
@@ -2,8 +2,10 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
-  DB_URL:
-    process.env.NODE_ENV === "test"
-      ? process.env.DB_TEST_URL
-      : process.env.DB_URL
+  DATABASE_URL:
+    process.env.DATABASE_URL ||
+    "postgresql://postgres@localhost/trivia_tracker",
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL ||
+    "postgresql://postgres@localhost/trivia_tracker_test"
 };
--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
 const app = require("./app");
 const knex = require("knex");
-const { PORT, DB_URL } = require("./config");
+const { PORT, DATABASE_URL } = require("./config");
 
 const db = knex({
   client: "pg",
-  connection: DB_URL
+  connection: DATABASE_URL
 });
 
 app.set("db", db);
 
 app.listen(PORT, () =>
-  console.log(`Listening on port ${PORT}`, "DB_URL:", DB_URL)
+  console.log(`Listening on port ${PORT}`, "DATABASE_URL:", DATABASE_URL)
 );
